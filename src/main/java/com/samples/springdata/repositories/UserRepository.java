cdf5cfa87faf0298331acfd777f8c6f6a8ff3f36
Vyacheslav Peshne <Vyacheslav.Peshne@magenta-technology.com>
2020-08-14 16:21:49 +0400

    update

--- src/main/java/com/samples/springdata/repositories/UserRepository.java
+++ src/main/java/com/samples/springdata/repositories/UserRepository.java
@@ -1,7 +1,7 @@
 package com.samples.springdata.repositories;
 
 import com.samples.springdata.entity.User;
-//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository {//extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 }
--- src/main/java/com/samples/springdata/service/UserService.java
+++ src/main/java/com/samples/springdata/service/UserService.java
@@ -10,61 +10,16 @@ import javax.annotation.PostConstruct;
 @Service
 public class UserService {
 
-    //@Autowired
-    UserRepository userRepository = new UserRepository() {
-        public <S extends User> S save(S s) {
-            return null;
-        }
-
-        public <S extends User> Iterable<S> save(Iterable<S> iterable) {
-            return null;
-        }
-
-        public User findOne(Integer integer) {
-            return null;
-        }
-
-        public boolean exists(Integer integer) {
-            return false;
-        }
-
-        public Iterable<User> findAll() {
-            return null;
-        }
-
-        public Iterable<User> findAll(Iterable<Integer> iterable) {
-            return null;
-        }
-
-        public long count() {
-            return 0;
-        }
-
-        public void delete(Integer integer) {
-
-        }
-
-        public void delete(User user) {
-
-        }
-
-        public void delete(Iterable<? extends User> iterable) {
-
-        }
-
-        public void deleteAll() {
-
-        }
-    };
+    @Autowired
+    UserRepository userRepository;
 
     @PostConstruct
     public void init() {
-        //userRepository.save(new User(1, "vasya"));
-        //userRepository.save(new User(2, "petya"));
+        userRepository.save(new User(1, "vasya"));
+        userRepository.save(new User(2, "petya"));
     }
 
     public User findUserById(Integer id) {
-        //return userRepository.findOne(id);
-        return new User(1, "123");
+        return userRepository.findOne(id);
     }
 }
